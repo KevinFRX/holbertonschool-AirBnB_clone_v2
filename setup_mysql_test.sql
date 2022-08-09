6b87bb4ea78ca894764e3a61fc1b0d6fd0c107fd
maurolabella <maurolabella@yahoo.com>
2022-08-08 23:26:47 -0300

    a

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -11,7 +11,7 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 -- to make changes effective
 FLUSH PRIVILEGES;
