53e9dfed4f259f121f34be166ab211798de2f70a
maurolabella <maurolabella@yahoo.com>
2022-08-10 14:15:40 -0300

    c

--- models/review.py
+++ models/review.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, Integer, String, Float, ForeignKey
 STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """Review class handles all application reviews"""
     if STORAGE_TYPE == "db":
         __tablename__ = 'reviews'
