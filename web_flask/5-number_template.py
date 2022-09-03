02de66785511d60a1cc3c91003b9c5939855447c
maurolabella <maurolabella@yahoo.com>
2022-09-03 17:52:45 -0300

    a

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
-""" Write a script that starts a Flask web application
-"""
+"""starts a Flask web app"""
+
 
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """/: display “Hello HBNB!”"""
+def index():
+    """set string to browser"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """/hbnb: display “HBNB”"""
+    """set string to browser"""
     return 'HBNB'
 
 
-@app.route('/c/<var>', strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def c(var):
-    """/c/<text>: display “C ”, followed by the value of the text variable
-    (replace underscore _ symbols with a space )"""
-    return 'C %s' % var.replace('_', ' ')
+    """capture variable"""
+    text = text.replace("_", " ")
+    return var
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python', defaults={'text': "is_cool"}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text="is cool"):
-    """/python/(<text>): display “Python ”, followed by the value of the text
-    variable (replace underscore _ symbols with a space )"""
-    return 'Python %s' % text.replace('_', ' ')
+def python(text):
+    """capture variable and set value"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def num(n):
-    """/number/<n>: display “n is a number” only if n is an integer"""
-    return '%d is a number' % n
+def number(n):
+    """is number or not"""
+    return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
@@ -48,4 +49,4 @@ def number_template(n):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host='0.0.0.0', port=5000)
