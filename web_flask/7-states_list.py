872415fa6545afb3914c1ae5ee0d1a89f8158caf
maurolabella <maurolabella@yahoo.com>
2022-09-05 18:20:17 -0300

    a

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
-
+from models.state import State
 from flask import Flask, render_template
 from models import storage
-from models.state import State
-from os import getenv
+"""from os import getenv"""
 
 
 app = Flask(__name__)
@@ -20,13 +19,14 @@ def close(exception):
 @app.route('/states_list')
 def states_list():
     """send object state to template"""
-    if (getenv('HBNB_TYPE_STORAGE') == 'db'):
-        states = storage.all(State)
-        states_list = []
-        for key, state in states.items():
-            states_list.append(state)
-        states_list.sort(key=lambda x: x.name)
-        return render_template("7-states_list.html", states=states_list)
+    """if (getenv('HBNB_TYPE_STORAGE') == 'db'):"""
+    states = storage.all(State)
+    states_list = []
+    for key, state in states.items():
+        states_list.append(state)
+    states_list.sort(key=lambda x: x.name)
+    return render_template("7-states_list.html", states=states_list)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
