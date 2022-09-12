4e4c89167eea90ff28312ab1e12d583c92a25da2
KevinFRX <xerokf13@gmail.com>
2022-09-12 02:26:21 +0000

    0.4

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,10 +3,8 @@
 
 sudo apt-get -y update
 sudo apt-get -y install nginx
-
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-
 sudo echo "<html>
   <head>
   </head>
@@ -14,7 +12,6 @@ sudo echo "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
