55e8968e9f8a2f0efafea0dde830760609e3a0e6
KevinFRX <xerokf13@gmail.com>
2022-09-15 23:47:18 +0000

    2.2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,10 +4,13 @@ Fabric script (based on the file 1-pack_web_static.py) that distributes
 an archive to your web servers, using the function do_deploy
 """
 
-from fabric.operations import local
+from fabric.operations import local, run, put
 from datetime import datetime
 import os
-from fabric.api import put, get, run
+from fabric.api import env
+
+
+env.hosts = ['54.91.233.56', '3.80.32.222']
 
 
 def do_pack():
@@ -27,45 +30,45 @@ def do_deploy(archive_path):
 
     upload = put(archive_path, '/tmp/')
     if upload.failed:
-        ret = False
+        return False
 
     filename = path.replace(".tgz", "").replace("versions/", "")
     uncompress = run('mkdir -p /data/web_static/releases/' + filename + '/')
     if uncompress.failed:
-        ret = False
+        return False
 
     unpack = run('tar -zfx /tmp/' + filename + '.tgz' +
                  ' -C /data/web_static/releases/' + filename + '/')
     if unpack.failed:
-        ret = False
+        return False
     
     cleanfile = run('rm /tmp/' + filename + '.tgz')
     if cleanfile.failed:
-        ret = False
+        return False
 
     move = run('cp -R /data/web_static/releases/' + filename +
                '/web_static/* /data/web_static/releases/' + filename +
                '/')
     if move.failed:
-        ret = False
+        return False
 
     delete_arch = run('rm -rf /data/web_static/releases/' + filename +
                  '/web_static')
     if delete_arch.failed:
-        ret = False
+        return False
 
     cleanfolder = run('rm -rf /data/web_static/releases/' + filename +
                       '/web_static')
     if cleanfolder.failed:
-        ret = False
+        return False
 
     delete_sym = run('rm -rf /data/web_static/current')
     if delete_sym.failed:
-        ret = False
+        return False
 
     new_sym = run('ln -sf /data/web_static/releases/' + filename +
                     '/' + ' /data/web_static/current')
     if new_sym.failed:
-        ret = False
+        return False
 
-    return ret
+    return True
