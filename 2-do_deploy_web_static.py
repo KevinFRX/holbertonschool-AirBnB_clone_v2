136526ea60cb7aaaca6b0523404742e553ffa9e9
KevinFRX <xerokf13@gmail.com>
2022-09-16 04:02:48 +0000

    2.5

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,68 +1,31 @@
 #!/usr/bin/python3
-"""
-Fabric script (based on the file 1-pack_web_static.py) that distributes
-an archive to your web servers, using the function do_deploy
-"""
-
-from fabric.operations import local, put, run
-from datetime import datetime
+"""Distributes an archive to your web servers, using the function do_deploy"""
+from fabric.contrib import files
+from fabric.api import env, put, run
 import os
-from fabric.api import env
-
 
 env.hosts = ['54.91.233.56', '3.80.32.222']
 
 
-def do_pack():
-    """comment"""
-    local("mkdir -p versions")
-    ret = local("tar zcvf versions/web_static_{}.tgz web_static"
-                .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
-    if ret.failed:
-        return None
-    else:
-        return ret
-
 def do_deploy(archive_path):
-    """comment"""
+    """Function for deploy"""
     if not os.path.exists(archive_path):
         return False
 
-    upload = put(archive_path, '/tmp/')
-    if upload.failed:
-        return False
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
 
-    filename = archive_path.split('.')[0]
-    filename = filename.split('/')[1]
-    uncompress = run('sudo mkdir -p /data/web_static/releases/{}/'.format(filename))
-    if uncompress.failed:
+    try:
+        put(archive_path, '/tmp')
+        run('sudo mkdir -p {}'.format(dest))
+        run('sudo tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
+        run('sudo rm -f /tmp/{}.tgz'.format(name))
+        run('sudo mv {}/web_static/* {}/'.format(dest, dest))
+        run('sudo rm -rf {}/web_static'.format(dest))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {} /data/web_static/current'.format(dest))
+        return True
+    except:
         return False
-
-    unpack = run('sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
-                 .format(filename, filename))
-    if unpack.failed:
-        return False
-
-    cleanfile = run('sudo rm -f /tmp/{}.tgz'.format(filename))
-    if cleanfile.failed:
-        return False
-
-    move = run('sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(filename, filename))
-    if move.failed:
-        return False
-
-    delete_arch = run('sudo rm -rf /data/web_static/releases/{}/web_static'
-                      .format(filename))
-    if delete_arch.failed:
-        return False
-
-    delete_sym = run('sudo rm -rf /data/web_static/current')
-    if delete_sym.failed:
-        return False
-
-    new_sym = run('sudo ln -s /data/web_static/releases/{}/ '
-                  '/data/web_static/current'.format(filename))
-    if new_sym.failed:
-        return False
-
-    return True
