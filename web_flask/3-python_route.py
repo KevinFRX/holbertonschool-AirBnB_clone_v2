bb9cf196d2ac431eb145e6f04676d4f7c89951ab
KevinFRX <xerokf13@gmail.com>
2022-09-19 12:05:26 -0700

    3.4

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -26,8 +26,8 @@ def c(text):
     return "C " + text
 
 
-@app.route('/python')
-@app.route('/python/<text>')
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python(text="is cool"):
     """comment"""
     text = text.repalce("_", " ")
