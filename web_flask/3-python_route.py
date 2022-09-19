e06201182d60f977511ee13201452a2017be6cec
KevinFRX <xerokf13@gmail.com>
2022-09-19 11:55:24 -0700

    3.1

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """comment"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """comment"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    """comment"""
+    text = text.replace("_", " ")
+    return "C " + text
+
+
+@app.route('/python/(<text>)')
+def python(text="is cool"):
+    """comment"""
+    text = text.repalce("_", " ")
+    return "Python " + text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
